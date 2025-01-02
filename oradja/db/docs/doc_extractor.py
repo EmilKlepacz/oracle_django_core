3fcac3d61805eee70a19d506f32c3bf16f39c83f
Emil Klepacz <klepaczemil@gmail.com>
2025-01-02 21:27:43 +0100

    DocExtractor: handling encoding for extracted data added.

--- oradja/db/docs/doc_extractor.py
+++ oradja/db/docs/doc_extractor.py
@@ -30,14 +30,23 @@ class DocExtractor:
         handler = file_handlers.get(self.file_type)
         return handler(doc_path)
 
-    def handle_pdf(self, doc_path: Path) -> dict:
+    def handle_pdf(self, doc_path: Path, encoding: str = "utf-8") -> dict:
         doc = pymupdf.open(doc_path)
 
         pages = []
         page_num = 0
         for page in doc:
-            # Get the raw JSON string in UTF-8
+            # Check if the page contains any selectable text
+            # in other case it is probably a scan (image only)
+            if page.get_text("text").strip():
+                has_text = True  # Document has selectable text
+
+            # Get the raw JSON string in UTF-8 by default
             page_raw_json = page.get_text("json")
+
+            if encoding.lower() != "utf-8":
+                page_raw_json = page_raw_json.encode("utf-8").decode(encoding=encoding, errors="replace")
+
             page_json = json.loads(page_raw_json)
 
             pages.append({
@@ -46,32 +55,38 @@ class DocExtractor:
             })
             page_num += 1
 
+        # If no selectable text was found
+        # (it is a scan so here some OCR needs to be used)
+        if not has_text:
+            # todo OCR logic to implement...
+            pass
+
         pages_json = {"pages": pages}
         return pages_json
 
-    def handle_bmp(self, doc_path: Path) -> dict:
+    def handle_bmp(self, doc_path: Path, encoding: str = "utf-8") -> dict:
         return {}
 
-    def handle_jpeg(self, doc_path: Path) -> dict:
+    def handle_jpeg(self, doc_path: Path, encoding: str = "utf-8") -> dict:
         return {}
 
-    def handle_doc(self, doc_path: Path) -> dict:
+    def handle_doc(self, doc_path: Path, encoding: str = "utf-8") -> dict:
         return {}
 
-    def handle_gif(self, doc_path: Path) -> dict:
+    def handle_gif(self, doc_path: Path, encoding: str = "utf-8") -> dict:
         return {}
 
-    def handle_jpg(self, doc_path: Path) -> dict:
+    def handle_jpg(self, doc_path: Path, encoding: str = "utf-8") -> dict:
         return {}
 
-    def handle_png(self, doc_path: Path) -> dict:
+    def handle_png(self, doc_path: Path, encoding: str = "utf-8") -> dict:
         return {}
 
-    def handle_tiff(self, doc_path: Path) -> dict:
+    def handle_tiff(self, doc_path: Path, encoding: str = "utf-8") -> dict:
         return {}
 
-    def handle_txt(self, doc_path: Path) -> dict:
+    def handle_txt(self, doc_path: Path, encoding: str = "utf-8") -> dict:
         return {}
 
-    def handle_xlsx(self, doc_path: Path) -> dict:
+    def handle_xlsx(self, doc_path: Path, encoding: str = "utf-8") -> dict:
         return {}
